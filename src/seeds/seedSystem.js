7c596a7e26c91da6b419491719ac800bb53db68c
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-04-10 00:24:15 +0700

    fix bug auto generate seeds system

--- src/seeds/seedSystem.js
+++ src/seeds/seedSystem.js
@@ -5,18 +5,23 @@ const seedSystem = async () => {
   const systemRepository = new MongoSystemRepository();
   const createSystem = new CreateSystemUsecase(systemRepository);
 
-  const systemData = {
-    name: "ILC",
-    address: "123 ABC",
-    phone: "123456789",
-    gmail: "a@gmail.com",
-    logo_url: "test_url",
-  };
+  const systems = [
+    {
+      name: "ILC",
+      address: "123 ABC",
+      phone: "123456789",
+      gmail: "a@gmail.com",
+      logo_url: "test_url",
+    },
+  ];
 
-  try {
-    await createSystem.executed(systemData);
-    console.log("create system success");
-  } catch (error) {}
+  for (const systemData of systems) {
+    try {
+      await createSystem.executed(systemData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 
 module.exports = seedSystem;
