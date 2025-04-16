3fbc975fb67f3830138e34d9a4f84cc282ad80ee
Azure2212 <dasanbob22122002@gmail.com>
2025-04-16 15:43:43 +0700

    update translation

--- src/routes/MemberRoutes.js
+++ src/routes/MemberRoutes.js
@@ -14,7 +14,7 @@ const MemberRoutes = () => {
   router.post("/", authMiddleware, (req, res) => {
     memberController.createMember(req, res);
   });
-  router.post("/translate/:id/:language", authMiddleware, (req, res) => {
+  router.post("/translate/:id/:fromLangue/:toLanguage", authMiddleware, (req, res) => {
     memberController.translateMember(req, res);
   });
 
